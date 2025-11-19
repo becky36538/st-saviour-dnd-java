60e28d649c98517356a81985f91c7cc4c57942cb
bmc2425 <recheriyan@stsaviour.org>
2025-11-18 21:21:21 -0500

    dnd maze

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -19,26 +19,21 @@ public class Game {
         System.out.println("Your name is " + name + " and your role is " + role + ".");
         
         // TODO Create character by collecting user input (name + role.
-
-
-
-        // TODO Print character sheet.
-
-        // Start the adventure.
-        // printDramaticText("Our adventure begins in a shady tavern ...");
+        // use dramatic text to give instructions to user
 
         printDramaticText("Our adventure begins in an empty, green, dark maze");
+        printDramaticText("A hungry Labubu has awoken, and is searching for you as a meal");
         printDramaticText("Use right, left, up, or down to escape");
         // Roll a d20
-        System.out.print("Press Enter to roll a d6.");
-        scanner.nextLine();
+        // System.out.print("Press Enter to roll a d6.");
+        // scanner.nextLine();
 
-        int roll = (int)(Math.random() * 6) + 1;
-        Ascii.drawD20(roll);
+        // int roll = (int)(Math.random() * 6) + 1;
+        // Ascii.drawD20(roll);
 
-        if(roll == 1 || roll == 2) {
-            printDramaticText("This is scenario 1");
-        }
+        // if(roll == 1 || roll == 2) {
+        //     printDramaticText("This is scenario 1");
+        // }
         
 
         // TODO Continue ...
@@ -61,38 +56,38 @@ public class Game {
     }
 
     // Draws a monster and returns an int which represents the difficulty of roll required.
-    public static int generateMonster() {
-        int r = (int)(Math.random() * 6) + 1;
-        if(r == 1 || r == 2 || r == 3) {
-            String zombie = Character.toString(0x1F9DF);
-            System.out.println("++++++++ " + zombie + " A HORDE OF ZOMBIES " + zombie + " ++++++++");
-            System.out.println("+                                        +");
-            System.out.println("+           roll required:  8            +");
-            System.out.println("+                                        +");
-            System.out.println("++++++++++++++++++++++++++++++++++++++++++");
+    // public static int generateMonster() {
+    //     int r = (int)(Math.random() * 6) + 1;
+    //     if(r == 1 || r == 2 || r == 3) {
+    //         String zombie = Character.toString(0x1F9DF);
+    //         System.out.println("++++++++ " + zombie + " A HORDE OF ZOMBIES " + zombie + " ++++++++");
+    //         System.out.println("+                                        +");
+    //         System.out.println("+           roll required:  8            +");
+    //         System.out.println("+                                        +");
+    //         System.out.println("++++++++++++++++++++++++++++++++++++++++++");
 
-            return 8;
-        }
-        if(r == 4 || r == 5) {
-            String mask = Character.toString(0x1F3AD);
-            System.out.println("++++++++++ " + mask + " DISGUISED MIMIC " + mask + " +++++++++");
-            System.out.println("+                                        +");
-            System.out.println("+           roll required:  12           +");
-            System.out.println("+                                        +");
-            System.out.println("++++++++++++++++++++++++++++++++++++++++++");
+    //         return 8;
+    //     }
+    //     if(r == 4 || r == 5) {
+    //         String mask = Character.toString(0x1F3AD);
+    //         System.out.println("++++++++++ " + mask + " DISGUISED MIMIC " + mask + " +++++++++");
+    //         System.out.println("+                                        +");
+    //         System.out.println("+           roll required:  12           +");
+    //         System.out.println("+                                        +");
+    //         System.out.println("++++++++++++++++++++++++++++++++++++++++++");
 
-            return 12;
-        }
-        if(r == 6) {
-            String eye = Character.toString(0x1F441);
-            System.out.println("+++++++++++ " + eye +  " EVIL BEHOLDER " + eye + " ++++++++++++");
-            System.out.println("+                                        +");
-            System.out.println("+           roll required:  18           +");
-            System.out.println("+                                        +");
-            System.out.println("++++++++++++++++++++++++++++++++++++++++++");
+    //         return 12;
+    //     }
+    //     if(r == 6) {
+    //         String eye = Character.toString(0x1F441);
+    //         System.out.println("+++++++++++ " + eye +  " EVIL BEHOLDER " + eye + " ++++++++++++");
+    //         System.out.println("+                                        +");
+    //         System.out.println("+           roll required:  18           +");
+    //         System.out.println("+                                        +");
+    //         System.out.println("++++++++++++++++++++++++++++++++++++++++++");
 
-            return 18;
-        }
-        return -1;
-    } 
+    //         return 18;
+    //     }
+    //     return -1;
+    // } 
 }
